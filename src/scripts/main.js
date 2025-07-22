4fdc5d7772b7d0b34c32a1ed9ed4ae1769ba19bf
Nazarii Korol <nazarkorol3684@gmail.com>
2025-07-22 15:37:54 +0300

    Message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const employers = [...document.body.querySelector('ul').children];
+
+function sortList(list) {
+  list.sort((a, b) => {
+    return convertSalary(b.dataset.salary) - convertSalary(a.dataset.salary);
+  });
+}
+
+function convertSalary(salary) {
+  return parseFloat(salary.replace(/[$,]/g, ''));
+}
+
+function getEmployees(list) {
+  return list.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: item.dataset.age,
+  }));
+}
+
+sortList(employers);
+
+getEmployees(employers);
